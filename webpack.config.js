fb671ec19189b714ee1d62dc438822853c9a7935
TanninOne <seppleviathan@gmx.de>
2021-07-14 15:10:59 +0200

    updated esptk to napi version

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,6 @@ let webpack = require('vortex-api/bin/webpack').default;
 config = webpack('gamebryo-plugin-management', __dirname, 4);
 
 config.externals['./build/Release/node-loot'] = './node-loot';
+config.externals['./build/Release/esptk'] = './esptk';
 
 module.exports = config;
